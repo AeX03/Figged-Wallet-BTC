33d1226c3ac58cdf3f325203e4942f45f562c1c7
AeX03 <103602164+AeX03@users.noreply.github.com>
2022-06-01 10:48:32 +0100

    Update Update.sh

--- Figged Wallet BTC/Update.sh
+++ Figged Wallet BTC/Update.sh
@@ -4,14 +4,14 @@
 
 ${GREEN}echo "                                                                                                                                                                                 ";
 ${GREEN}echo "oooooooooooo  o8o                                        .o8       oooooo   oooooo     oooo           oooo  oooo                .        oooooooooo.  ooooooooooooo   .oooooo.   "; 
-${GREEN}echo "`888'     `8  `"'                                       "888        `888.    `888.     .8'            `888  `888              .o8        `888'   `Y8b 8'   888   `8  d8P'  `Y8b  ";
+${GREEN}echo "`888'     `8  `''                                       '888        `888.    `888.     .8'            `888  `888              .o8        `888'   `Y8b 8'   888   `8  d8P'  `Y8b  ";
 ${GREEN}echo " 888         oooo   .oooooooo  .oooooooo  .ooooo.   .oooo888         `888.   .8888.   .8'    .oooo.    888   888   .ooooo.  .o888oo       888     888      888      888          ";
 ${GREEN}echo " 888oooo8    `888  888' `88b  888' `88b  d88' `88b d88' `888          `888  .8'`888. .8'    `P  )88b   888   888  d88' `88b   888         888oooo888'      888      888          ";
-${GREEN}echo " 888    "     888  888   888  888   888  888ooo888 888   888           `888.8'  `888.8'      .oP"888   888   888  888ooo888   888         888    `88b      888      888          ";
+${GREEN}echo " 888    '     888  888   888  888   888  888ooo888 888   888           `888.8'  `888.8'      .oP'888   888   888  888ooo888   888         888    `88b      888      888          ";
 ${GREEN}echo " 888          888  `88bod8P'  `88bod8P'  888    .o 888   888            `888'    `888'      d8(  888   888   888  888    .o   888 .       888    .88P      888      `88b    ooo  ";
-${GREEN}echo "o888o        o888o `8oooooo.  `8oooooo.  `Y8bod8P' `Y8bod88P"            `8'      `8'       `Y888""8o o888o o888o `Y8bod8P'   "888"      o888bood8P'      o888o      `Y8bood8P'  ";
-${GREEN}echo "                   d"     YD  d"     YD                                                                                                                                          ";
-${GREEN}echo "                   "Y88888P'  "Y88888P'                                                                                                                                          ";
+${GREEN}echo "o888o        o888o `8oooooo.  `8oooooo.  `Y8bod8P' `Y8bod88P'            `8'      `8'       `Y888""8o o888o o888o `Y8bod8P'   "888"      o888bood8P'      o888o      `Y8bood8P'  ";
+${GREEN}echo "                   d'     YD  d'     YD                                                                                                                                          ";
+${GREEN}echo "                   'Y88888P'  'Y88888P'                                                                                                                                          ";
 ${GREEN}echo "                                                                                                                                                                                 ";
 
 
